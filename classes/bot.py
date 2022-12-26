a0869f85f834a406bbefe8a880b5a5eb1412022c
DeadRayst <mr.darmelow@gmail.com>
2022-12-26 23:58:16 +0400

    Added keyboard

--- classes/bot.py
+++ classes/bot.py
@@ -3,23 +3,27 @@ from typing import List
 
 from aiogram import Bot as AIOBot, Dispatcher, executor, types
 
-import emoji
+from classes.keyboard import KeyBoard
+
+from utils.emoji import emojize
 
 
 class Bot:
-    __bot_commands: List[str] = ['start', 'help', 'low', 'high', 'custom', 'history', 'game_rating']
+    __bot_commands: List[str] = [
+        'start', 'help', 'low',
+        'high', 'custom', 'history',
+        'game_rating', 'settings'
+    ]
 
     def __init__(self) -> None:
         self.__aioBot = AIOBot(token=os.getenv('BOT_TOKEN'))
         self.__dispatcher = Dispatcher(self.__aioBot)
+        self.__keyboard = KeyBoard(self, self.__dispatcher)
 
     def start(self) -> str:
         self.write_history('start')
-
-        msg = 'Приветствую!!! Я могу показать тебе рейтинг игр. Только выбери команду ниже '
-        msg_emoji = emoji.emojize(':backhand_index_pointing_down:', variant='emoji_type')
-
-        return msg + msg_emoji
+        return 'Приветствую!!! Я могу показать тебе рейтинг игр. ' \
+               'Только выбери команду ниже ' + emojize(':backhand_index_pointing_down:')
 
     def get_help(self) -> str:
         self.write_history('help')
@@ -43,39 +47,40 @@ class Bot:
     def write_history(self, command: str) -> None:
         pass
 
+    def settings(self) -> str:
+        return 'settings'
+
     def get_game_rating(self) -> str:
         self.write_history('game_rating')
         return 'Mafia: 7\nGod of war: 9'
+
+    def watch_command(self) -> None:
+        @self.__dispatcher.message_handler(commands=self.__bot_commands)
+        async def command(msg: types.Message) -> None:
+            match msg.text:
+                case '/start':
+                    await msg.answer(self.start(), reply_markup=self.__keyboard.get_main())
+
+                case '/help': await msg.answer(self.get_help())
+                case '/low': await msg.answer(self.set_low())
+                case '/high': await msg.answer(self.set_high())
+                case '/custom': await msg.answer(self.set_custom())
+                case '/game_rating': await msg.answer(self.get_game_rating())
+                case '/history': await msg.answer(self.get_history())
+                case '/settings':
+                    await msg.answer(self.settings(), reply_markup=self.__keyboard.get_settings())
+
+                case _: await msg.answer('Простите, но данная команда не существует.')
 
     def run(self) -> None:
         async def on_startup(_: any) -> None:
             print('Бот включился')
 
-        @self.__dispatcher.message_handler(commands=self.__bot_commands)
-        async def command(msg: types.Message) -> None:
-            match msg.text:
-                case '/start':
-                    await msg.answer(self.start())
+        self.watch_command()
+        self.__keyboard.watch_keyboard()
 
-                case '/help':
-                    await msg.answer(self.get_help())
-
-                case '/low':
-                    await msg.answer(self.set_low())
-
-                case '/high':
-                    await msg.answer(self.set_high())
-
-                case '/custom':
-                    await msg.answer(self.set_custom())
-
-                case '/game_rating':
-                    await msg.answer(self.get_game_rating())
-
-                case '/history':
-                    await msg.answer(self.get_history())
-
-                case _:
-                    await msg.answer('Простите, но данная команда не существует.')
-
-        executor.start_polling(self.__dispatcher, skip_updates=True, on_startup=on_startup)
+        executor.start_polling(
+            self.__dispatcher,
+            skip_updates=True,
+            on_startup=on_startup
+        )
